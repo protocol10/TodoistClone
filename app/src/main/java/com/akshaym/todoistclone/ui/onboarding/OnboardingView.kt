554bb06707ac56f02467e7d2768814a18f27e97a
protocol10 <mukadam.akshay@gmail.com>
2026-03-07 13:55:25 +0530

    feat: Designed onboarding view for todoist app. Adds social button widgets used in onboarding view and adds assets used in onboarding screen

--- app/src/main/java/com/akshaym/todoistclone/ui/onboarding/OnboardingView.kt
+++ app/src/main/java/com/akshaym/todoistclone/ui/onboarding/OnboardingView.kt
@@ -1,9 +1,135 @@
 package com.akshaym.todoistclone.ui.onboarding
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withLink
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.akshaym.todoistclone.R
+import com.akshaym.todoistclone.ui.widgets.SocialButtons
 
 @Composable
-fun OnboardingView(modifier: Modifier) {
+fun OnboardingView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.str_organize_content_tag),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, bottom = 0.dp, end = 16.dp, top = 32.dp),
+            fontSize = 24.sp,
+            fontFamily = FontFamily.Monospace,
+        )
 
+        Image(
+            painter = painterResource(id = R.drawable.ic_onboarding),
+            contentDescription = stringResource(R.string.str_content_description_onboarng_image),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(vertical = 64.dp),
+        )
+
+        Spacer(modifier = Modifier.weight(0.1f))
+
+        SocialButtons(
+            onClick = { },
+            imageVector = Icons.Default.Email,
+            contentDescription = stringResource(R.string.str_content_description_email),
+            iconPadding = 8.dp,
+            tintColor = Color.White,
+            buttonText = stringResource(R.string.str_continue_email)
+        )
+//        Button(onClick = {}, modifier = Modifier.fillMaxWidth(), content = {
+//            // Used for showing specifically for icons i.e smaller icons and by default is 24 X 24
+//            // Officially recommended for icon buttons, if we want more flexible then we can use Image composable
+//            Icon(
+//                imageVector = Icons.Default.Email,
+//                contentDescription = "Google",
+//                modifier = Modifier.padding(start = 0.dp, end = 8.dp),
+//                tint = Color.White // used to change the color of the icon originally its black
+//            )
+//            Text("Continue with Email", textAlign = TextAlign.Center)
+//        })
+        SocialButtons(
+            onClick = { },
+            resourceId = R.drawable.ic_google,
+            contentDescription = stringResource(R.string.str_content_description_google),
+            iconPadding = 8.dp,
+            tintColor = Color.White,
+            buttonText = stringResource(R.string.str_continue_google)
+        )
+        SocialButtons(
+            onClick = { },
+            resourceId = R.drawable.ic_facebook,
+            contentDescription = stringResource(R.string.str_content_description_facebook),
+            iconPadding = 8.dp,
+            tintColor = Color.White,
+            buttonText = stringResource(R.string.str_continue_facebook)
+        )
+
+        val termsAndPrivacyText = buildAnnotatedString {
+            append(stringResource(R.string.str_terms_agreement))
+            withLink(
+                LinkAnnotation.Url(
+                    stringResource(R.string.str_link_terms_condition), styles = TextLinkStyles(
+                        style = SpanStyle(
+                            color = Color.Blue, fontWeight = FontWeight.Bold
+                        )
+                    )
+                ), {
+                    append(stringResource(R.string.str_terms_of_service))
+                })
+            append(" and ")
+            withLink(
+                LinkAnnotation.Url(
+                    stringResource(R.string.str_link_privacy), styles = TextLinkStyles(
+                        style = SpanStyle(
+                            color = Color.Blue, fontWeight = FontWeight.Bold
+                        )
+                    )
+                ), {
+                    append(stringResource(R.string.str_privacy))
+                })
+            append(".")
+        }
+        Text(
+            termsAndPrivacyText, modifier = Modifier.padding(vertical = 16.dp)
+        )
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    OnboardingView()
 }
---
+++ app/src/main/java/com/akshaym/todoistclone/ui/widgets/SocialButtons.kt
@@ -0,0 +1,88 @@
+package com.akshaym.todoistclone.ui.widgets
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+/**
+ * A custom button component that displays an icon followed by text.
+ * This version uses an [ImageVector] for the icon.
+ *
+ * @param onClick Callback to be invoked when the button is clicked.
+ * @param imageVector The [ImageVector] to be displayed as an icon.
+ * @param contentDescription Accessibility description for the icon.
+ * @param iconPadding The spacing between the icon and the button text.
+ * @param tintColor The color to apply to the icon (defaults to [Color.White]).
+ * @param buttonText The text to be displayed on the button.
+ */
+@Composable
+fun SocialButtons(
+    onClick: () -> Unit,
+    imageVector: ImageVector,
+    contentDescription: String,
+    iconPadding: Dp,
+    tintColor: Color = Color.White,
+    buttonText: String
+) {
+    Button(onClick = onClick, modifier = Modifier.fillMaxWidth(), content = {
+        // Used for showing specifically for icons i.e smaller icons and by default is 24 X 24
+        // Officially recommended for icon buttons, if we want more flexible then we can use Image composable
+        Icon(
+            imageVector = imageVector,
+            contentDescription = contentDescription,
+            modifier = Modifier
+                .padding(start = 0.dp, end = iconPadding)
+                .size(16.dp),
+            tint = tintColor // used to change the color of the icon originally its black
+        )
+        Text(buttonText, textAlign = TextAlign.Center)
+    })
+}
+
+/**
+ * A custom button component that displays an icon followed by text.
+ * This version uses a drawable resource ID for the icon.
+ *
+ * @param onClick Callback to be invoked when the button is clicked.
+ * @param resourceId The [DrawableRes] ID of the icon to be displayed.
+ * @param contentDescription Accessibility description for the icon.
+ * @param iconPadding The spacing between the icon and the button text.
+ * @param tintColor The color to apply to the icon (defaults to [Color.White]).
+ * @param buttonText The text to be displayed on the button.
+ */
+@Composable
+fun SocialButtons(
+    onClick: () -> Unit,
+    @DrawableRes resourceId: Int,
+    contentDescription: String,
+    iconPadding: Dp,
+    tintColor: Color = Color.White,
+    buttonText: String
+) {
+    Button(onClick = onClick, modifier = Modifier.fillMaxWidth(), content = {
+        // Used for showing specifically for icons i.e smaller icons and by default is 24 X 24
+        // Officially recommended for icon buttons, if we want more flexible then we can use Image composable
+        Icon(
+            painter = painterResource(resourceId),
+            contentDescription = contentDescription,
+            modifier = Modifier
+                .padding(start = 0.dp, end = iconPadding)
+                .size(16.dp),
+            tint = tintColor // used to change the color of the icon originally its black
+        )
+        Text(buttonText, textAlign = TextAlign.Center)
+    })
+}
