8fd51acf7a99af7ad12e683e02b11effb8042b40
protocol10 <mukadam.akshay@gmail.com>
2026-03-07 13:58:23 +0530

    chore: Removed unused code

--- app/src/main/java/com/akshaym/todoistclone/ui/onboarding/OnboardingView.kt
+++ app/src/main/java/com/akshaym/todoistclone/ui/onboarding/OnboardingView.kt
@@ -2,16 +2,12 @@ package com.akshaym.todoistclone.ui.onboarding
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
